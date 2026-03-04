0dcaa62f2d3bc7096f3178b3cbeacb56f35a97d9
YYYXB <1746155023@qq.com>
2026-03-04 21:50:02 +0800

    2026-3-4 21:49:39 WPF行为

---
+++ WPF/Basic/BehaviorDemo/EffectBehavior.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace BehaviorDemo
+{
+    /// <summary>
+    /// 鼠标选中的UI高亮
+    /// </summary>
+    public class EffectBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseMove += AssociatedObject_MouseMove;
+            AssociatedObject.MouseLeave += AssociatedObject_MouseLeave;
+        }
+
+
+        private void AssociatedObject_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            FrameworkElement? element = sender as FrameworkElement;
+            element!.Effect = (Effect)new DropShadowEffect()
+            {
+                Color = Colors.Red,
+                ShadowDepth = 0,
+            };
+        }
+        private void AssociatedObject_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            FrameworkElement? element = sender as FrameworkElement;
+            element!.Effect = (Effect)new DropShadowEffect()
+            {
+                Color = Colors.Transparent,
+                ShadowDepth = 0,
+            };
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.MouseMove -= AssociatedObject_MouseMove;
+            AssociatedObject.MouseLeave -= AssociatedObject_MouseLeave;
+        }
+    }
+}
